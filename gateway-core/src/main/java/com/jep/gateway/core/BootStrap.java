d0607a2a341e6b069a64755acfee8f89ca0cfd0b
enping <58761173@qq.com>
2025-01-27 20:52:41 +0800

    commit

---
+++ gateway-core/src/main/java/com/jep/gateway/core/BootStrap.java
@@ -0,0 +1,13 @@
+package com.jep.gateway.core;
+
+/**
+ * @author enping.jep
+ * @date 2025/1/27 18:57
+ **/
+public class BootStrap {
+
+    public static void main(String[] args) {
+
+    }
+
+    }
